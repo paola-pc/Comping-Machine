5df01fc2b7f21a48a44eb1533c2bea72b89ee1dd
Paola <paola.patino.castillo@gmail.com>
2023-04-21 22:51:31 +0200

    Scrollbar change

--- fullstack/src/components/Layout.jsx
+++ fullstack/src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "./layout/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className=" bg-black w-screen">
+    <div className=" bg-black ">
       <div className="container h-full mx-auto xl:px-30 px-20">
         <div className=" mx-auto w-full">
           {/* if needed add another div here for responsiveness */}
