3b60c6fe46f1092cc57fcdebad2a9e2021dd5b4e
Paola <paola.patino.castillo@gmail.com>
2023-05-24 19:34:50 +0200

    head and title fix

--- fullstack/src/pages/_app.js
+++ fullstack/src/pages/_app.js
@@ -2,12 +2,18 @@ import 'bring/styles/globals.css';
 import Layout from '../components/Layout'
 import { SessionProvider, useSession } from 'next-auth/react';
 import LoginModal from 'bring/components/modals/loginModal';
+import React from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps, session }) {
   
 
   return (
     <>
+      <Head>
+        <title>Comping Machine</title>
+        <link rel="icon" href='/logo-over-black.png' />
+      </Head>
       <SessionProvider session={session}>
         <LoginModal></LoginModal>
         <Layout>
--- fullstack/src/pages/_document.js
+++ fullstack/src/pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Main, NextScript, Head } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-        <title>Comping Machine</title>
-        <link rel="icon" href='/logo-over-black.png'/>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
